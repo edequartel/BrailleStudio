71efb623f994cda3039e42b98ff56aa8808fc2c4
Eric de Quartel <edequartel@protonmail.com>
2026-03-24 09:13:45 +0100

    update

--- api/list.php
+++ api/list.php
@@ -177,6 +177,15 @@ OPMERKINGEN
 */
 
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Vary: Origin');
+
+if (($_SERVER['REQUEST_METHOD'] ?? 'GET') === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
 
 // -----------------------------
 // Input
