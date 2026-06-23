b8fcf2501283869781a6c29deba761cc13f64c79
Eric de Quartel <edequartel@protonmail.com>
2026-06-23 19:32:25 +0200

    up

--- blockly-library/save.php
+++ blockly-library/save.php
@@ -5,6 +5,6 @@ require_once __DIR__ . '/../api/json-guard.php';
 
 braillestudio_json_guard_run(static function (): void {
     require_once __DIR__ . '/../auth/bootstrap.php';
-    bs_auth_require_login(['admin', 'docent'], 'json');
+    bs_auth_require_login(['admin', 'developer'], 'json');
     require __DIR__ . '/../api/blockly-library/save.php';
 });
